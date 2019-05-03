426c40332c721c7ff155daa41bfa817043a68fe9
J. P <101110@vivaldi.net>
2019-05-03 11:31:23 +0200

    Update scripts, begin work on R710 script

--- Dell/R710-script.sh
+++ Dell/R710-script.sh
@@ -4,52 +4,37 @@
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:
 
 # Set the location of the file we will be dumping the IPMITOOL output to.
-IPMIOUT="/tmp/ipmi-logger/ipmiout-dl320-g6"
+IPMIOUT="/tmp/ipmi-logger/ipmiout-r710"
 
 GRAPHITE_HOST="<GRAPHITE-HOST-IP>"
 
 # Pull the data we want from the HP machine, Via IPMITOOL
 /usr/bin/ipmitool -I lanplus -H <TARGET-IP> -U <TARGET_IPMI_USERNAME> -P <TARGET_IPMI_PASSWORD> sdr elist full > ${IPMIOUT}
+/usr/bin/ipmitool -I lanplus -H <TARGET-IP> -U <TARGET_IPMI_USERNAME> -P <TARGET_IPMI_PASSWORD> sensors > ${IPMIOUT}-1
 
 # Select what we are sending to Graphite
-FAN_1="$(grep "Fan 1" ${IPMIOUT} | awk ' {print $10} ')"
-FAN_2="$(grep "Fan 2" ${IPMIOUT} | awk ' {print $10} ')"
-FAN_3="$(grep "Fan 3" ${IPMIOUT} | awk ' {print $10} ')"
-FAN_4="$(grep "Fan 4" ${IPMIOUT} | awk ' {print $10} ')"
-FAN_5="$(grep "Fan 5" ${IPMIOUT} | awk ' {print $10} ')"
-FAN_6="$(grep "Fan 6" ${IPMIOUT} | awk ' {print $10} ')"
-FAN_7="$(grep "Fan 7" ${IPMIOUT} | awk ' {print $10} ')"
-AMBI_TEMP_1="$(grep -w "Temp 1" ${IPMIOUT} | awk ' {print $10} ')"
-CPU1_TEMP="$(grep -w "Temp 2" ${IPMIOUT} | awk ' {print $10} ')"
-CPU2_TEMP="$(grep -w "Temp 3" ${IPMIOUT} | awk ' {print $10} ')"
-MEM_ZONE_1="$(grep -w "Temp 4" ${IPMIOUT} | awk ' {print $10} ')"
-MEM_ZONE_2="$(grep -w "Temp 5" ${IPMIOUT} | awk ' {print $10} ')"
-MEM_ZONE_3="$(grep -w "Temp 6" ${IPMIOUT} | awk ' {print $10} ')"
-MEM_ZONE_4="$(grep -w "Temp 7" ${IPMIOUT} | awk ' {print $10} ')"
-MEM_ZONE_5="$(grep -w "Temp 8" ${IPMIOUT} | awk ' {print $10} ')"
-MEM_ZONE_6="$(grep -w "Temp 9" ${IPMIOUT} | awk ' {print $10} ')"
-MEM_ZONE_7="$(grep -w "Temp 10" ${IPMIOUT} | awk ' {print $10} ')"
-MEM_ZONE_8="$(grep -w "Temp 11" ${IPMIOUT} | awk ' {print $10} ')"
-MEM_ZONE_9="$(grep -w "Temp 12" ${IPMIOUT} | awk ' {print $10} ')"
-MEM_ZONE_10="$(grep -w "Temp 13" ${IPMIOUT} | awk ' {print $10} ')"
-MEM_ZONE_11="$(grep -w "Temp 14" ${IPMIOUT} | awk ' {print $10} ')"
-MEM_ZONE_12="$(grep -w "Temp 15" ${IPMIOUT} | awk ' {print $10} ')"
-SYS_ZONE_1="$(grep -w "Temp 16" ${IPMIOUT} | awk ' {print $10} ')"
-SYS_ZONE_2="$(grep -w "Temp 17" ${IPMIOUT} | awk ' {print $10} ')"
-SYS_ZONE_3="$(grep -w "Temp 18" ${IPMIOUT} | awk ' {print $10} ')"
-SYS_ZONE_4="$(grep -w "Temp 19" ${IPMIOUT} | awk ' {print $10} ')"
-SYS_ZONE_5="$(grep -w "Temp 20" ${IPMIOUT} | awk ' {print $10} ')"
-SYS_ZONE_6="$(grep -w "Temp 21" ${IPMIOUT} | awk ' {print $10} ')"
-SYS_ZONE_7="$(grep -w "Temp 22" ${IPMIOUT} | awk ' {print $10} ')"
-SYS_ZONE_8="$(grep -w "Temp 23" ${IPMIOUT} | awk ' {print $10} ')"
-SYS_ZONE_9="$(grep -w "Temp 24" ${IPMIOUT} | awk ' {print $10} ')"
-AMBI_TEMP_2="$(grep -w "Temp 25" ${IPMIOUT} | awk ' {print $10} ')"
-SYS_ZONE_10="$(grep -w "Temp 26" ${IPMIOUT} | awk ' {print $10} ')"
-SYS_ZONE_11="$(grep -w "Temp 27" ${IPMIOUT} | awk ' {print $10} ')"
-SYS_ZONE_12="$(grep -w "Temp 28" ${IPMIOUT} | awk ' {print $10} ')"
-STOR_ZONE="$(grep -w "Temp 29" ${IPMIOUT} | awk ' {print $10} ')"
-AMBI_ZONE_3="$(grep -w "Temp 30" ${IPMIOUT} | awk ' {print $10} ')"
-AMBI_ZONE_4="$(grep -w "Temp 31" ${IPMIOUT} | awk ' {print $10} ')"
+FAN_1="$(grep "FAN 1" ${IPMIOUT} | awk ' {print $11} ')"
+FAN_2="$(grep "FAN 2" ${IPMIOUT} | awk ' {print $11} ')"
+FAN_3="$(grep "FAN 3" ${IPMIOUT} | awk ' {print $11} ')"
+FAN_4="$(grep "FAN 4" ${IPMIOUT} | awk ' {print $11} ')"
+FAN_5="$(grep "FAN 5" ${IPMIOUT} | awk ' {print $11} ')"
+AMBI_TEMP="$(grep -w "Ambient Temp" ${IPMIOUT} | awk ' {print $10} ')"
+PLAN_TEMP="$(grep -w "Planar Temp" ${IPMIOUT} | awk ' {print $10} ')"
+POWER_METER="$(grep -w "System Level" ${IPMIOUT} | awk ' {print $10} ')"
+VOLT1="$(grep -w "96h" ${IPMIOUT} | awk ' {print $9} ')"
+VOLT2="$(grep -w "97h" ${IPMIOUT} | awk ' {print $9} ')"
+AMP1="$(grep -w "94h" ${IPMIOUT} | awk ' {print $9} ')"
+AMP2="$(grep -w "95h" ${IPMIOUT} | awk ' {print $9} ')"
+TEMP1="$(grep -w "01h" ${IPMIOUT} | awk ' {print $9} ')"
+TEMP2="$(grep -w "02h" ${IPMIOUT} | awk ' {print $9} ')"
+TEMP3="$(grep -w "05h" ${IPMIOUT} | awk ' {print $9} ')"
+TEMP4="$(grep -w "06h" ${IPMIOUT} | awk ' {print $9} ')"
+TEMP5="$(grep -w "0Ah" ${IPMIOUT} | awk ' {print $9} ')"
+TEMP6="$(grep -w "0Bh" ${IPMIOUT} | awk ' {print $9} ')"
+TEMP7="$(grep -w "0Ch" ${IPMIOUT} | awk ' {print $9} ')"
+
+WATT1="$(echo '${VOLT1}*${AMP1}' | bc)"
+WATT2="$(echo '${VOLT2}*${AMP2}' | bc)"
 
 # Magic time. Detect losses etc.
 PSU_PRESENCE="$(grep "Power Supply 1" ${IPMIOUT} | awk ' {print $11} ')"
@@ -66,13 +51,11 @@ else
 fi
 
 # Sent to Graphite
-echo "dl320-g6.FAN.BLOCK_1 ${FAN_1}" | nc -q0 ${GRAPHITE_HOST} 2003
-echo "dl320-g6.FAN.BLOCK_2 ${FAN_2}" | nc -q0 ${GRAPHITE_HOST} 2003
-echo "dl320-g6.FAN.BLOCK_3 ${FAN_3}" | nc -q0 ${GRAPHITE_HOST} 2003
-echo "dl320-g6.FAN.BLOCK_4 ${FAN_4}" | nc -q0 ${GRAPHITE_HOST} 2003
-echo "dl320-g6.FAN.BLOCK_5 ${FAN_5}" | nc -q0 ${GRAPHITE_HOST} 2003
-echo "dl320-g6.FAN.BLOCK_6 ${FAN_6}" | nc -q0 ${GRAPHITE_HOST} 2003
-echo "dl320-g6.FAN.BLOCK_7 ${FAN_7}" | nc -q0 ${GRAPHITE_HOST} 2003
+echo "dl320-g6.FAN.RPM_1 ${FAN_1}" | nc -q0 ${GRAPHITE_HOST} 2003
+echo "dl320-g6.FAN.RPM_2 ${FAN_2}" | nc -q0 ${GRAPHITE_HOST} 2003
+echo "dl320-g6.FAN.RPM_3 ${FAN_3}" | nc -q0 ${GRAPHITE_HOST} 2003
+echo "dl320-g6.FAN.RPM_4 ${FAN_4}" | nc -q0 ${GRAPHITE_HOST} 2003
+echo "dl320-g6.FAN.RPM_5 ${FAN_5}" | nc -q0 ${GRAPHITE_HOST} 2003
 echo "dl320-g6.FAN.REDUN ${FAN_REDUN}" | nc -q0 ${GRAPHITE_HOST} 2003
 echo "dl320-g6.AMBI1_TEMP ${AMBI_TEMP_1}" | nc -q0 ${GRAPHITE_HOST} 2003
 echo "dl320-g6.AMBI2_TEMP ${AMBI_TEMP_2}" | nc -q0 ${GRAPHITE_HOST} 2003
